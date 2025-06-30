9036c35767a0125ce231ed5b0bcb678983715faa
EnesDemir143 <enesd41@outlook.com>
2025-06-30 15:04:05 +0300

    dataloader test done.

--- DataLoader/dataloader.py
+++ DataLoader/dataloader.py
@@ -4,17 +4,26 @@ from torch.utils.data import Dataset
 import pandas as pd
 import os
 from glob import glob
+from torchvision import transforms
+from PIL import Image
+
 
 class chestXrayDataset(Dataset):
-    def __init__(self, img_path, datainfo_path, transform=None, scaler=None):
+    def __init__(self, img_path, datainfo_path):
         self.img_path = img_path
         self.datainfo_path = datainfo_path
         self.data_csv = pd.read_csv(self.datainfo_path)
-        
-        self.transform = transform
-        self.scaler = scaler
 
+        self.label_map  = {"PA": 0, "AP": 1}
+        
         self.images = glob(os.path.join(self.img_path))
+
+        self.transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+
 
         img_filenames = [os.path.basename(img_path) for img_path in self.images]
         self.matching_rows = self.data_csv[self.data_csv['Image Index'].isin(img_filenames)]
@@ -24,3 +33,13 @@ class chestXrayDataset(Dataset):
     def __len__(self):
         return len(self.images)
     
+    def __getitem__(self, idx):
+        img_path = self.images[idx]
+        label_str = self.img_labels[idx]
+        
+        img = Image.open(img_path).convert("L")
+        img_tensor = self.transform(img)
+
+        label = self.label_map.get(label_str)
+
+        return img_tensor, label
